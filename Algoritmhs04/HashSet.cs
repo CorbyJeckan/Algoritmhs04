fb5daf271515635eafa62e1ca9f75895f69f4794
Евгений Савельев <curtisjeckan@mail.ru>
2022-09-01 13:31:48 +0500

    выполнил практическое задание к уроку 4

---
+++ Algoritmhs04/HashSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmhs04
+{
+    internal class HashSet
+    {
+        public static void MainHash()
+        {
+            Random r = new Random();
+            HashSet<string> myHash = new HashSet<string>();
+            while (myHash.Count < 10_000)
+            {
+                string value = default;
+                for (int j = 0; j < r.Next(5, 10); j++) value += ((char)r.Next('a', 'z')).ToString();
+                myHash.Add(value);
+            }
+            //Замер HasSet
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            foreach (var item in myHash)
+            {
+                if (item.Equals("abcderfdk"))
+                {
+                    break;
+                }
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string time = $"{ts.Hours}:{ts.Minutes}:{ts.Seconds}:{ts.Milliseconds / 10}";
+            Console.WriteLine("RunTime " + time);
+            
+        }
+    }
+}
--- Algoritmhs04/Program.cs
+++ Algoritmhs04/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using Algoritmhs04;
+using System;
+
+
+namespace Algorithms
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Title = "Algorithms";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Здравствуйте!\nПожалуйста,введите пункт и подпункт для выбора домашнего задания.\nДоступные для выбора пункты:\n1 - Протестируйте поиск строки в HashSet и в массиве.\n2 - Реализуйте двоичное дерево и метод вывода его в консоль." );
+            string selectednumber = (Console.ReadLine());
+            switch (selectednumber)
+            {
+                case "1":
+                    {
+                        HashSet.MainHash();
+                    }
+                    break;
+
+                case "2":
+                    {
+                        Pprogram.MainTwo();
+
+                    }
+                   break;
+
+
+
+            }
+        }
+    }
+
+}
